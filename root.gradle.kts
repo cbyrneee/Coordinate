caf216d655c88da4e62df5350877dd882bc24af5
dreamhopping <71222289+dreamhopping@users.noreply.github.com>
2021-04-03 22:09:03 +0100

    Move root project to its own subproject

---
+++ root.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    kotlin("jvm") version "1.4.31" apply false
+    kotlin("plugin.serialization") version "1.4.31" apply false
+    `maven-publish`
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
-rootProject.name = "Coordinate"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.buildFileName = "root.gradle.kts"
+
+include("main")
 include("intellij-plugin")
 include("swing-app")
