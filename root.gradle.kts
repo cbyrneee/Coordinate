15aa8168c6a504b54ca612d0e80598c9394d6642
dreamhopping <71222289+dreamhopping@users.noreply.github.com>
2021-05-13 21:45:37 +0100

    [Build] Update to Kotlin 1.5.0

--- root.gradle.kts
+++ root.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.4.31" apply false
-    kotlin("plugin.serialization") version "1.4.31" apply false
+    kotlin("jvm") version "1.5.0" apply false
+    kotlin("plugin.serialization") version "1.5.0" apply false
     `maven-publish`
 }
